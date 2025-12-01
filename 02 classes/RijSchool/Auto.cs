47366645fb79b14b56e5dbe636cf943426e49704
QO-O <39372@ma-web.nl>
2025-12-01 09:50:38 +0100

    Commit for myu teacher to figure out my mistakes

--- 02 classes/RijSchool/Auto.cs
+++ 02 classes/RijSchool/Auto.cs
@@ -4,6 +4,6 @@ internal class Auto{
     internal bool automaat;
     internal string merk;
     internal string kenteken;
-    internal int kilometerStand
+    internal int kilometerStand;
 
 }
--- 02 classes/RijSchool/Program.cs
+++ 02 classes/RijSchool/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program program = new Program();
+        program.Run();
     }
+    Auto auto1 = new Auto()
+        {
+        automaat = true,
+        kenteken = "AA824H",
+        kilometerStand = 20567,
+        merk = "Volkswagen"
+        };
+
+        Auto auto2 = new Auto()
+        {
+        automaat = false,
+        kenteken = "KU786G",
+        kilometerStand = 65739,
+        merk = "Volvo"
+        };
+
+    RijLeraar rijLeraar = new RijLeraar()
+        {
+        leeftijd = 30,
+        naam = "John",
+        zzp = false
+        };
+
+    LesUur lesUur = new LesUur()
+        {
+        auto = auto1,
+        rijLeraar = rijLeraar,
+        tijd = 1130
+        };
+
+    Student student = new Student()
+    {
+       
+    };
 }
+
