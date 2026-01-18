51a88eac2e4c29b4a31158489b1793f01ffde7a7
QO-O <39372@ma-web.nl>
2026-01-18 10:59:03 +0100

    Start of 05

---
+++ 05 dictionaries/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _05_dictionaries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+
+        Dictionary<int, string> personeelOpNummer = new Dictionary<int, string>();
+
+        personeelOpNummer.Add(382942, "Jim");
+        personeelOpNummer.Add(789432, "Tim");
+
+        string Jim = personeelOpNummer[382942];
+
+        foreach (KeyValuePair<int, string> item in personeelOpNummer)
+        {
+            int nummer = item.Key;
+            string naam = item.Value;
+            Console.WriteLine(nummer + ": " + naam);
+        }
+    }
+     internal void Run()
+    {
+
+    }
+}
